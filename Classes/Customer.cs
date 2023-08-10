d3caa7c10a757dbb0ea4f467e7a7e9b7a9c68629
AHMETTASDELEN <ahmettasdelen5499@gmail.com>
2023-08-10 22:48:20 +0300

    transferring from computer

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -7,7 +7,7 @@ namespace Classes
     class Customer
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } 
         public string LastName { get; set; }
         public string City { get; set; }
     }
